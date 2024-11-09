994c3b0c3598c0d73cc3fe10be2b804381e47419
KevinRamlow <kevinandraderamlow@gmail.com>
2024-11-09 15:07:59 -0300

    estilizar página home

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -1,13 +1,40 @@
-// components/pages/home/home.js
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './home.css';
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div className='container'>
-      <p className='title'>Love App</p>  
-    </div>
+      <div className='container'>
+        <p className='title'>Love App</p>
+
+        <hr className='divider divider1' />
+      
+        <p className='introduction'>
+          Amor, nesses dois anos ao seu lado, meu coração se encheu de alegria e felicidade a cada dia. 
+          Você torna minha vida mais doce e especial. Eu só quero continuar ao seu lado, compartilhando muitos
+          outros momentos incríveis. Feliz dois anos de nós minha princesa!
+        </p>
+
+        <hr className='divider divider2' />
+
+        <div className='button-column'>
+        <button className='btn' onClick={() => navigate('/histories')}>
+          <span className='btn-text'>Histórias</span>
+        </button>
+        <button className='btn' onClick={() => navigate('/poems')}>
+          <span className='btn-text'>Poemas</span>
+        </button>
+        <button className='btn' onClick={() => navigate('/musics')}>
+          <span className='btn-text'>Músicas</span>
+        </button>
+        <button className='btn' onClick={() => navigate('/declaration')}>
+          <span className='btn-text'>Declaração</span>
+        </button>
+        </div>
+      </div>
   );
 }
 
