065e14de6c597ad804e08b9bcfb16e0cf6708211
KevinRamlow <kevinandraderamlow@gmail.com>
2024-11-08 23:44:33 -0300

    teste navigate das paginas

--- src/components/pages/musics/musics.js
+++ src/components/pages/musics/musics.js
@@ -1,7 +1,22 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 function Musics () {
+  const navigate = useNavigate();
+
+  const navigateToPoems = () => {
+    navigate('/poems');
+  };
+
   return (
     <div>
-      <h1>Musics</h1>
+      <h1>Home</h1>
+      <button 
+        onClick={navigateToPoems} 
+        style={{ backgroundColor: 'red', borderRadius: '0', color: 'white', padding: '10px 20px', border: 'none' }}
+      >
+        Go to Poems
+      </button>
     </div>
   );
 }
