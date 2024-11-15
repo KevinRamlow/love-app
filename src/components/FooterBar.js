041c247e1f51aa3daeb43937760deda9516be2b5
KevinRamlow <kevinandraderamlow@gmail.com>
2024-11-15 12:53:57 -0300

    add imgs to footbar

--- src/components/FooterBar.js
+++ src/components/FooterBar.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './footerBar.css';
 
+const images = {
+  home: require('../img/footBarImages/home.png'),
+  instagram: require('../img/footBarImages/instagram.png'),
+  gitHub: require('../img/footBarImages/gitHub.png'),
+  linkedin: require('../img/footBarImages/linkedin.png'),
+  portfolio: require('../img/footBarImages/portfolio.png')
+};
+
 function FooterBar() {
   const navigate = useNavigate();
   const [showPopup, setShowPopup] = useState(false);
@@ -26,19 +34,19 @@ function FooterBar() {
   return (
     <div className="footer-bar">
       <button className="footer-icon" onClick={() => window.open('https://www.instagram.com/sk.an07/', '_blank')}>
-        <img src="/footBarImages/instagram.png" alt="Instagram" />
+        <img src={images.instagram} alt="Instagram" />
       </button>
       <button className="footer-icon" onClick={() => window.open('https://github.com/KevinRamlow', '_blank')}>
-        <img src="/footBarImages/gitHub.png" alt="GitHub" />
+        <img src={images.gitHub} alt="GitHub" />
       </button>
       <button className="footer-icon" onClick={() => navigate('/')}>
-        <img src="/footBarImages/home.png" alt="Home" />
+        <img src={images.home} alt="Home" />
       </button>
       <button className="footer-icon" onClick={() => window.open('https://www.linkedin.com/in/kevin-ramlow-028509281/', '_blank')}>
-        <img src="/footBarImages/linkedin.png" alt="Linkedin" />
+        <img src={images.linkedin} alt="Linkedin" />
       </button>
       <button className="footer-icon" onClick={handlePortfolioClick}>
-        <img src="/footBarImages/portfolio.png" alt="Portfolio" />
+        <img src={images.portfolio} alt="Portfolio" />
       </button>
 
       {showPopup && (
